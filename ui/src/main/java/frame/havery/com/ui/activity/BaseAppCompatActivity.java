96c4e4b3c7ad785b2bf51c12f8574b330e93e6f0
wyh497823256@qq.com <497823256@qq.com>
2016-03-24 11:28:09 +0800

    新增UI
    新增UI

---
+++ ui/src/main/java/frame/havery/com/ui/activity/BaseAppCompatActivity.java
@@ -0,0 +1,431 @@
+package frame.havery.com.ui.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+import frame.havery.com.ui.BaseAppManager;
+import frame.havery.com.ui.R;
+import frame.havery.com.ui.utils.CommonUtils;
+import frame.havery.com.ui.utils.SmartBarUtils;
+import frame.havery.com.ui.viewbind.AnnotateUtil;
+import frame.havery.com.ui.widget.loading.VaryViewHelperController;
+
+
+/**
+ * Created by fangxiao on 15/12/18.
+ */
+public abstract class BaseAppCompatActivity extends AppCompatActivity implements View.OnClickListener {
+    /**
+     * Log tag
+     */
+    protected static String TAG_LOG = null;
+    /**
+     * 屏幕参数
+     */
+    protected int mScreenWidth = 0;
+    protected int mScreenHeight = 0;
+    protected float mScreenDensity = 0.0f;
+
+    /**
+     * 上下文
+     */
+    protected Context mContext = null;
+
+
+    /**
+     * loading view controller
+     */
+    private VaryViewHelperController mVaryViewHelperController = null;
+
+    /**
+     * overridePendingTransition mode
+     */
+    public enum TransitionMode {
+        LEFT, RIGHT, TOP, BOTTOM, SCALE, FADE
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (toggleOverridePendingTransition()) {
+            switch (getOverridePendingTransitionMode()) {
+                case LEFT:
+                    overridePendingTransition(R.anim.left_in, R.anim.left_out);
+                    break;
+                case RIGHT:
+                    overridePendingTransition(R.anim.right_in, R.anim.right_out);
+                    break;
+                case TOP:
+                    overridePendingTransition(R.anim.top_in, R.anim.top_out);
+                    break;
+                case BOTTOM:
+                    overridePendingTransition(R.anim.bottom_in, R.anim.bottom_out);
+                    break;
+                case SCALE:
+                    overridePendingTransition(R.anim.scale_in, R.anim.scale_out);
+                    break;
+                case FADE:
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                    break;
+            }
+        }
+        super.onCreate(savedInstanceState);
+        // base setup
+        Bundle extras = getIntent().getExtras();
+        if (null != extras) {
+            getBundleExtras(extras);
+        }
+
+
+        SmartBarUtils.hide(getWindow().getDecorView());
+        setTranslucentStatus(isApplyStatusBarTranslucency());
+
+        mContext = this;
+        TAG_LOG = this.getClass().getSimpleName();
+        BaseAppManager.getInstance().addActivity(this);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        mScreenDensity = displayMetrics.density;
+        mScreenHeight = displayMetrics.heightPixels;
+        mScreenWidth = displayMetrics.widthPixels;
+
+        if (getContentViewLayoutID() != 0) {
+            setContentView(getContentViewLayoutID());
+        } else {
+            throw new IllegalArgumentException("You must return a right contentView layout resource Id");
+        }
+
+
+        initViewsAndEvents();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        AnnotateUtil.initBindView(this);
+        if (null != getLoadingTargetView()) {
+            mVaryViewHelperController = new VaryViewHelperController(getLoadingTargetView());
+        }
+    }
+
+    public void widgetClick(View v) {
+    }
+
+    @Override
+    public void onClick(View v) {
+        widgetClick(v);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        BaseAppManager.getInstance().removeActivity(this);
+        if (toggleOverridePendingTransition()) {
+            switch (getOverridePendingTransitionMode()) {
+                case LEFT:
+                    overridePendingTransition(R.anim.left_in, R.anim.left_out);
+                    break;
+                case RIGHT:
+                    overridePendingTransition(R.anim.right_in, R.anim.right_out);
+                    break;
+                case TOP:
+                    overridePendingTransition(R.anim.top_in, R.anim.top_out);
+                    break;
+                case BOTTOM:
+                    overridePendingTransition(R.anim.bottom_in, R.anim.bottom_out);
+                    break;
+                case SCALE:
+                    overridePendingTransition(R.anim.scale_in, R.anim.scale_out);
+                    break;
+                case FADE:
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                    break;
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    protected <T extends View> T bindView(int id) {
+        return (T) findViewById(id);
+    }
+
+    @SuppressWarnings("unchecked")
+    protected <T extends View> T bindView(int id, boolean click) {
+        T view = (T) findViewById(id);
+        if (click) {
+            view.setOnClickListener(this);
+        }
+        return view;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    /**
+     * get bundle data
+     *
+     * @param extras
+     */
+    protected abstract void getBundleExtras(Bundle extras);
+
+    /**
+     * bind layout resource file
+     *
+     * @return id of layout resource
+     */
+    protected abstract int getContentViewLayoutID();
+
+    /**
+     * init all views and add events
+     */
+    protected abstract void initViewsAndEvents();
+
+
+    /**
+     * get loading target view
+     */
+    protected abstract View getLoadingTargetView();
+
+
+    /**
+     * is applyStatusBarTranslucency
+     *
+     * @return
+     */
+    protected abstract boolean isApplyStatusBarTranslucency();
+
+
+    /**
+     * toggle overridePendingTransition
+     *
+     * @return
+     */
+    protected abstract boolean toggleOverridePendingTransition();
+
+    /**
+     * get the overridePendingTransition mode
+     */
+    protected abstract TransitionMode getOverridePendingTransitionMode();
+
+    /**
+     * startActivity
+     *
+     * @param clazz
+     */
+    protected void readyGo(Class<?> clazz) {
+        Intent intent = new Intent(this, clazz);
+        startActivity(intent);
+    }
+
+    /**
+     * startActivity with bundle
+     *
+     * @param clazz
+     * @param bundle
+     */
+    protected void readyGo(Class<?> clazz, Bundle bundle) {
+        Intent intent = new Intent(this, clazz);
+        if (null != bundle) {
+            intent.putExtras(bundle);
+        }
+        startActivity(intent);
+    }
+
+    /**
+     * startActivity then finish
+     *
+     * @param clazz
+     */
+    protected void readyGoThenKill(Class<?> clazz) {
+        Intent intent = new Intent(this, clazz);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * startActivity with bundle then finish
+     *
+     * @param clazz
+     * @param bundle
+     */
+    protected void readyGoThenKill(Class<?> clazz, Bundle bundle) {
+        Intent intent = new Intent(this, clazz);
+        if (null != bundle) {
+            intent.putExtras(bundle);
+        }
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * startActivityForResult
+     *
+     * @param clazz
+     * @param requestCode
+     */
+    protected void readyGoForResult(Class<?> clazz, int requestCode) {
+        Intent intent = new Intent(this, clazz);
+        startActivityForResult(intent, requestCode);
+    }
+
+    /**
+     * startActivityForResult with bundle
+     *
+     * @param clazz
+     * @param requestCode
+     * @param bundle
+     */
+    protected void readyGoForResult(Class<?> clazz, int requestCode, Bundle bundle) {
+        Intent intent = new Intent(this, clazz);
+        if (null != bundle) {
+            intent.putExtras(bundle);
+        }
+        startActivityForResult(intent, requestCode);
+    }
+
+    /**
+     * show toast
+     *
+     * @param msg
+     */
+    protected void showToast(String msg) {
+        //防止遮盖虚拟按键
+        if (null != msg && !CommonUtils.isEmpty(msg)) {
+            Snackbar.make(getLoadingTargetView(), msg, Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * toggle show loading
+     *
+     * @param toggle
+     */
+    protected void toggleShowLoading(boolean toggle, String msg) {
+        if (null == mVaryViewHelperController) {
+            throw new IllegalArgumentException("You must return a right target view for loading");
+        }
+
+        if (toggle) {
+            mVaryViewHelperController.showLoading(msg);
+        } else {
+            mVaryViewHelperController.restore();
+        }
+    }
+
+    /**
+     * toggle show empty
+     *
+     * @param toggle
+     */
+    protected void toggleShowEmpty(boolean toggle, String msg, View.OnClickListener onClickListener) {
+        if (null == mVaryViewHelperController) {
+            throw new IllegalArgumentException("You must return a right target view for loading");
+        }
+
+        if (toggle) {
+            mVaryViewHelperController.showEmpty(msg, onClickListener);
+        } else {
+            mVaryViewHelperController.restore();
+        }
+    }
+
+    /**
+     * toggle show error
+     *
+     * @param toggle
+     */
+    protected void toggleShowError(boolean toggle, String msg, View.OnClickListener onClickListener) {
+        if (null == mVaryViewHelperController) {
+            throw new IllegalArgumentException("You must return a right target view for loading");
+        }
+
+        if (toggle) {
+            mVaryViewHelperController.showError(msg, onClickListener);
+        } else {
+            mVaryViewHelperController.restore();
+        }
+    }
+
+    /**
+     * toggle show network error
+     *
+     * @param toggle
+     */
+    protected void toggleNetworkError(boolean toggle, View.OnClickListener onClickListener) {
+        if (null == mVaryViewHelperController) {
+            throw new IllegalArgumentException("You must return a right target view for loading");
+        }
+
+        if (toggle) {
+            mVaryViewHelperController.showNetworkError(onClickListener);
+        } else {
+            mVaryViewHelperController.restore();
+        }
+    }
+
+
+    /**
+     * use SytemBarTintManager
+     *
+     * @param tintDrawable
+     */
+    protected void setSystemBarTintDrawable(Drawable tintDrawable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            SystemBarTintManager mTintManager = new SystemBarTintManager(this);
+            if (tintDrawable != null) {
+                mTintManager.setStatusBarTintEnabled(true);
+                mTintManager.setTintDrawable(tintDrawable);
+            } else {
+                mTintManager.setStatusBarTintEnabled(false);
+                mTintManager.setTintDrawable(null);
+            }
+        }
+
+    }
+
+    /**
+     * set status bar translucency
+     *
+     * @param on
+     */
+    protected void setTranslucentStatus(boolean on) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window win = getWindow();
+            WindowManager.LayoutParams winParams = win.getAttributes();
+            final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            if (on) {
+                winParams.flags |= bits;
+            } else {
+                winParams.flags &= ~bits;
+            }
+            win.setAttributes(winParams);
+        }
+    }
+
+
+}
---
+++ ui/src/main/java/frame/havery/com/ui/activity/BaseSwipeBackCompatActivity.java
@@ -0,0 +1,54 @@
+package frame.havery.com.ui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import frame.havery.com.ui.widget.swipeback.SwipeBackActivityBase;
+import frame.havery.com.ui.widget.swipeback.SwipeBackActivityHelper;
+import frame.havery.com.ui.widget.swipeback.SwipeBackLayout;
+import frame.havery.com.ui.widget.swipeback.Utils;
+
+
+/**
+ * Created by fangxiao on 15/12/18.
+ */
+public abstract class BaseSwipeBackCompatActivity extends BaseAppCompatActivity implements SwipeBackActivityBase {
+    private SwipeBackActivityHelper mHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHelper = new SwipeBackActivityHelper(this);
+        mHelper.onActivityCreate();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mHelper.onPostCreate();
+    }
+
+    @Override
+    public View findViewById(int id) {
+        View v = super.findViewById(id);
+        if (v == null && mHelper != null)
+            return mHelper.findViewById(id);
+        return v;
+    }
+
+    @Override
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mHelper.getSwipeBackLayout();
+    }
+
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    @Override
+    public void scrollToFinishActivity() {
+        Utils.convertActivityToTranslucent(this);
+        getSwipeBackLayout().scrollToFinishActivity();
+    }
+}
