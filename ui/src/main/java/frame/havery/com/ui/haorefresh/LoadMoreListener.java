19d87d732b5af001bd288ce4b6fe66bb184d4121
wyh497823256@qq.com <497823256@qq.com>
2016-03-24 17:32:24 +0800

    增加HaoRecyclview

---
+++ ui/src/main/java/frame/havery/com/ui/haorefresh/LoadMoreListener.java
@@ -0,0 +1,13 @@
+package frame.havery.com.ui.haorefresh;
+
+/**
+ * Created by fangxiao on 15/12/2.
+ */
+public interface LoadMoreListener {
+
+    /**
+     * loading more
+     */
+    void onLoadMore();
+
+}
